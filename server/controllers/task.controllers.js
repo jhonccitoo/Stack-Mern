85729d8ad1678306a2262b618e597404850644ea
John Roberto Paquirachi Yanama <129339472+jhonccitoo@users.noreply.github.com>
2025-03-31 22:37:38 +0000

    crenado el sistema

---
+++ server/controllers/task.controllers.js
@@ -0,0 +1,15 @@
+export const getTasks=(req,res)=>{
+res.send("obtenido tareas")
+}
+export const getTask=(req,res)=>{
+res.send("obtenido una tarea")
+}
+export const createTask=(req,res)=>{
+res.send("creando tareas")
+}
+export const updateTask=(req,res)=>{
+res.send("actualizando tareas")
+}
+export const deleteTask=(req,res)=>{
+res.send("eliminando tarea")
+}
---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+import indexRoutes from "./routes/index.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+
+const app = express();
+
+app.use(indexRoutes);
+app.use(taskRoutes);
+
+app.listen(PORT);
+console.log(`server is listening on port ${PORT}`);
